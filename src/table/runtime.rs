69410c62c2e6f656ffb9734ffee999c4b96de240
Justus Polzin <justuspolzin@gmail.com>
2020-04-05 23:09:35 +0200

    Add variable services

--- src/table/runtime.rs
+++ src/table/runtime.rs
@@ -2,10 +2,14 @@
 
 use super::Header;
 use crate::table::boot::MemoryDescriptor;
-use crate::{Result, Status};
+use crate::{Result, Status, Guid};
+use crate::data_types::Char16;
 use bitflags::bitflags;
 use core::mem::MaybeUninit;
 use core::ptr;
+use core::ffi::c_void;
+#[cfg(feature = "exts")]
+use crate::alloc_api::{string::String, vec, vec::Vec};
 
 /// Contains pointers to all of the runtime services.
 ///
@@ -17,15 +21,38 @@ pub struct RuntimeServices {
     get_time:
         unsafe extern "efiapi" fn(time: *mut Time, capabilities: *mut TimeCapabilities) -> Status,
     set_time: unsafe extern "efiapi" fn(time: &Time) -> Status,
-    // Skip some useless functions.
-    _pad: [usize; 2],
+    get_wakeup_time: usize,
+    set_wakeup_time: usize,
     set_virtual_address_map: unsafe extern "efiapi" fn(
         map_size: usize,
         desc_size: usize,
         desc_version: u32,
         virtual_map: *mut MemoryDescriptor,
     ) -> Status,
-    _pad2: [usize; 5],
+    convert_pointer: extern "efiapi" fn(
+        debug_disposition: usize,
+        address: &mut *const u8
+    ) -> Status,
+    get_variable: extern "efiapi" fn(
+        variable_name: *const Char16,
+        vendor_guid: &Guid,
+        attributes: *mut u32,
+        data_size: &mut usize,
+        data: *mut c_void
+    ) -> Status,
+    get_next_variable_name: extern "efiapi" fn(
+        variable_name_size: &mut usize,
+        variable_name: *mut Char16,
+        vendor_guid: &mut Guid
+    ) -> Status,
+    set_variable: extern "efiapi" fn(
+        variable_name: *const Char16,
+        vendor_guid: &Guid,
+        attributes: u32,
+        data_size: usize,
+        data: *const c_void
+    ) -> Status,
+    get_next_high_monotonic_count: usize,
     reset: unsafe extern "efiapi" fn(
         rt: ResetType,
 
@@ -81,6 +108,66 @@ impl RuntimeServices {
         (self.set_virtual_address_map)(map_size, entry_size, entry_version, map_ptr).into()
     }
 
+    /// Returns an iterator over EFI variables.
+    #[cfg(feature = "exts")]
+    pub fn variables<'a>(&'a self) -> VariablesIterator {
+        VariablesIterator::new(self)
+    }
+
+    /// Used internally by VariablesIterator.
+    fn get_next_variable_name(&self, variable_name: &mut [u16], vendor_guid: &mut Guid) -> (Status, usize) {
+        let mut buf_len = variable_name.len();
+        let status = (self.get_next_variable_name)(&mut buf_len, variable_name as *mut _ as *mut Char16, vendor_guid);
+
+        (status, buf_len)
+    }
+
+    /// Get the data stored with the variable.
+    #[cfg(feature = "exts")]
+    pub fn get_variable(&self, variable: &Variable) -> Result<(Vec<u8>, u32)> {
+        let mut attributes = 0;
+        let name_len = variable.name.as_str().chars().count();
+        let mut name = vec![0; name_len + 1];
+
+        ucs2::encode(variable.name.as_str(), &mut name[..name_len])
+            .map_err(|_| Status::INVALID_PARAMETER)?;
+
+        // use some default size to try to avoid a second call because of buffer size
+        let mut data = vec![0; 64];
+        let mut data_size = data.len();
+
+        // this might be called when multiple processors are running, so the data could get bigger
+        // even after resizing the buffer, so this should be called in a loop
+        loop {
+            match (self.get_variable)(name.as_ptr() as *const Char16, &variable.vendor, 
+                &mut attributes, &mut data_size, data.as_mut_ptr() as *mut c_void) {
+                Status::SUCCESS => {
+                    // resize the vector to reflect the size of the data returned
+                    data.truncate(data_size);
+                    return Ok(crate::Completion::new(Status::SUCCESS, (data, attributes)));
+                },
+                Status::BUFFER_TOO_SMALL => {
+                    // resize the buffer 
+                    data.resize(data_size, 0);
+                },
+                s => return Err(s.into()),
+            }
+        }
+    }
+
+    /// Sets the value of a variable. This service can be used to create a new variable, 
+    /// modify the value of an existing variable, or to delete an existing variable.
+    pub fn set_variable(&self, variable: &Variable, attributes: u32, data: &[u8]) -> Result {
+        let name_len = variable.name.as_str().chars().count();
+        let mut name = vec![0; name_len + 1];
+
+        ucs2::encode(variable.name.as_str(), &mut name[..name_len])
+            .map_err(|_| Status::INVALID_PARAMETER)?;
+
+        (self.set_variable)(name.as_ptr() as *const Char16, &variable.vendor, 
+            attributes, data.len(), data.as_ptr() as *const c_void).into()
+    }
+
     /// Resets the computer.
     pub fn reset(&self, rt: ResetType, status: Status, data: Option<&[u8]>) -> ! {
         let (size, data) = match data {
@@ -100,6 +187,89 @@ impl RuntimeServices {
 
 impl super::Table for RuntimeServices {
     const SIGNATURE: u64 = 0x5652_4553_544e_5552;
+}
+
+/// An EFI Variable
+#[cfg(feature = "exts")]
+#[derive(Debug, Clone)]
+pub struct Variable {
+    /// The variable name.
+    pub name: String,
+
+    /// The vendor's guid.
+    pub vendor: Guid,
+}
+
+#[cfg(feature = "exts")]
+impl Variable {
+    /// Creates an EFI Variable from a variable name and the vendor guid.
+    pub fn new(name: String, vendor: Guid) -> Variable {
+        Variable {
+            name,
+            vendor
+        }
+    }
+}
+
+/// An iterator for EFI variables.
+#[cfg(feature = "exts")]
+pub struct VariablesIterator<'a> {
+    rt: &'a RuntimeServices,
+    buffer: Vec<u16>,
+    guid: Guid,
+}
+
+#[cfg(feature = "exts")]
+impl<'a> VariablesIterator<'a> {
+    /// Creates an iterator over EFI Variables.
+    pub fn new(runtime_services: &'a RuntimeServices) -> VariablesIterator {
+        VariablesIterator {
+            rt: runtime_services,
+            buffer: vec![0; 32], // try to avoid BUFFER_TOO_SMALL
+            guid: Guid::from_values(0, 0, 0, 0, [0, 0, 0, 0, 0, 0]),
+        }
+    }
+}
+
+#[cfg(feature = "exts")]
+impl<'a> Iterator for VariablesIterator<'a> {
+    type Item = Variable;
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.rt.get_next_variable_name(self.buffer.as_mut_slice(), &mut self.guid) {
+            (Status::SUCCESS, _) => (),
+            (Status::BUFFER_TOO_SMALL, len) => {
+                // Note that if EFI_BUFFER_TOO_SMALL is returned,
+                // the VariableName buffer was too small for the next variable. 
+                // When such an error occurs, the VariableNameSize is updated 
+                // to reflect the size of buffer needed.
+                self.buffer.resize(len, 0);
+
+                let (status, _) = self.rt.get_next_variable_name(self.buffer.as_mut_slice(), &mut self.guid);
+                if status.is_error() {
+                    panic!("get_next_variable failed after resizing the buffer ({:?})", status);
+                }
+            }
+            _ => return None,
+        }
+
+        let name_len = self.buffer.as_slice().iter().take_while(|&&e| e != 0).count();
+
+        let mut utf8_buffer = vec![0; name_len * 3];
+
+        // every ucs2 string has a utf8 encoding so this should never fail
+        let name_len = ucs2::decode(&self.buffer[..name_len], &mut utf8_buffer[..])
+            .expect("get_next_variable_name returned a string that couldn't be converted to utf8");
+
+        // truncate the vector so that we can give it to the String constructor
+        utf8_buffer.truncate(name_len);
+
+        // ucs2::decode only returns valid utf8 encodings
+        let name = unsafe {
+            String::from_utf8_unchecked(utf8_buffer)
+        };
+
+        return Some(Variable::new(name, self.guid));
+    }
 }
 
 /// The current time information
